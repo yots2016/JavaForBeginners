7da748fe1736775079c44b9a3e1c3435e72be68b
Viacheslav <newstageja@gmail.com>
2018-07-20 23:36:35 +0300

    task 6 is completed

---
+++ src/main/java/slack_task_06/Main.java
@@ -0,0 +1,29 @@
+package slack_task_06;
+
+import java.util.*;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+    private final static int ARRAY_SIZE = 10;
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        Integer randomNumbersArray[] = new Integer[ARRAY_SIZE];
+
+        for (int i = 0; i < ARRAY_SIZE; i++) {
+            randomNumbersArray[i] = random.nextInt(21) - 10;
+        }
+
+        for (int number : randomNumbersArray) {
+            System.out.print(" " + number);
+            System.out.println();
+        }
+
+        List<Integer> randomNumbersList = Arrays.asList(randomNumbersArray);
+
+        System.out.println("Max = " + Collections.max(randomNumbersList));
+        System.out.println("Min = " + Collections.min(randomNumbersList));
+    }
+}
