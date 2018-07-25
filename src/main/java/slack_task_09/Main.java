5d661ca539f415cfd9669258f7ff6a7e84076797
Viacheslav Otamas <newstageja@gmail.com>
2018-07-25 15:46:08 +0300

    modified task 8 is completed

--- src/main/java/slack_task_09/Main.java
+++ src/main/java/slack_task_09/Main.java
@@ -1,18 +1,27 @@
 package slack_task_09;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new double[] {1.3, .5, 89.000005, 12}));
-        System.out.println();
-        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new short[] {3, 5, 5, 12, 555}));
-        System.out.println();
-        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new long[] {3, 1000, 678678, 4654}));
-        System.out.println();
-        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(12, 3));
-        System.out.println();
-        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(54544f, 76f));
-        System.out.println();
+import java.util.Arrays;
+import java.util.Random;
 
+public class Main {
+    private static final Random RANDOM = new Random(10);
+
+    public static void main(String[] args) {
+//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new double[]{1.3, .5, 89.000005, 12}));
+//        System.out.println();
+//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new short[]{3, 5, 5, 12, 555}));
+//        System.out.println();
+//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new long[]{3, 1000, 678678, 4654}));
+//        System.out.println();
+//        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(12, 3));
+//        System.out.println();
+//        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(54544f, 76f));
+//        System.out.println();
+
+        int[] randomNumbersArray = new int[17];
+
+        randomArrayFill(randomNumbersArray, randomNumbersArray.length);
+        System.out.println(Arrays.toString(randomNumbersArray));
     }
 
     private static int getAverageArithmeticValue(int[] inputArray) {
@@ -83,5 +92,13 @@ public class Main {
         return firstNumber * firstNumber + secondNumber * secondNumber;
     }
 
+    private static void randomArrayFill(int inputArray[], int arrayLength) {
+        if (arrayLength == 0) {
+            return;
+        } else {
+            randomArrayFill(inputArray,arrayLength - 1);
+        }
 
+        inputArray[--arrayLength] = RANDOM.nextInt(10);
+    }
 }
