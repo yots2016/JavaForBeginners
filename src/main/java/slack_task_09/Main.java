691d905f099b01c3e68552c4bbbbad8f49c081c5
Viacheslav <newstageja@gmail.com>
2018-07-26 00:13:16 +0300

    some fix HW #9

--- src/main/java/slack_task_09/Main.java
+++ src/main/java/slack_task_09/Main.java
@@ -7,24 +7,24 @@ public class Main {
     private static final Random RANDOM = new Random(10);
 
     public static void main(String[] args) {
-//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new double[]{1.3, .5, 89.000005, 12}));
-//        System.out.println();
-//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new short[]{3, 5, 5, 12, 555}));
-//        System.out.println();
-//        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new long[]{3, 1000, 678678, 4654}));
-//        System.out.println();
-//        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(12, 3));
-//        System.out.println();
-//        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(54544f, 76f));
-//        System.out.println();
+        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new double[]{1.3, .5, 89.000005, 12}));
+        System.out.println();
+        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new short[]{3, 5, 5, 12, 555}));
+        System.out.println();
+        System.out.println("AverageArithmetic = " + getAverageArithmeticValue(new long[]{3, 1000, 678678, 4654}));
+        System.out.println();
+        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(12, 3));
+        System.out.println();
+        System.out.println("The sum of squares of two numbers = " + getSumOfSquaresOfTwoNumbers(54544f, 76f));
+        System.out.println();
 
-        int[] randomNumbersArray = new int[17];
+        int[] randomNumbersArray = new int[10];
 
         randomArrayFill(randomNumbersArray, randomNumbersArray.length);
         System.out.println(Arrays.toString(randomNumbersArray));
     }
 
-    private static int getAverageArithmeticValue(int[] inputArray) {
+    private static long getAverageArithmeticValue(int[] inputArray) {
         int sum = 0;
 
         for (int anInputArray : inputArray) {
@@ -44,7 +44,7 @@ public class Main {
         return sum / inputArray.length;
     }
 
-    private static int getAverageArithmeticValue(byte[] inputArray) {
+    private static long getAverageArithmeticValue(byte[] inputArray) {
         int sum = 0;
 
         for (byte anInputArray : inputArray) {
@@ -54,7 +54,7 @@ public class Main {
         return sum / inputArray.length;
     }
 
-    private static int getAverageArithmeticValue(short[] inputArray) {
+    private static long getAverageArithmeticValue(short[] inputArray) {
         int sum = 0;
 
         for (short anInputArray : inputArray) {
@@ -84,7 +84,7 @@ public class Main {
         return sum / inputArray.length;
     }
 
-    private static int getSumOfSquaresOfTwoNumbers(int firstNumber, int secondNumber) {
+    private static long getSumOfSquaresOfTwoNumbers(int firstNumber, int secondNumber) {
         return firstNumber * firstNumber + secondNumber * secondNumber;
     }
 
