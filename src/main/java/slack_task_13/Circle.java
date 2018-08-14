f47f554e2eb99cac5e11ea119e724e0b287a7a3a
Viacheslav <newstageja@gmail.com>
2018-08-14 23:17:31 +0300

    added comments

--- src/main/java/slack_task_13/Circle.java
+++ src/main/java/slack_task_13/Circle.java
@@ -1,20 +1,25 @@
 package slack_task_13;
 
 public class Circle {
+    // Конструктор класу
     public Circle(double radius) {
         this.radius = radius;
     }
 
+    // Поле класу
     private double radius;
 
+    // Метод, що обраховує та повертає площу
     public double getSquare(){
         return Math.PI * Math.pow(radius, 2);
     }
 
+    // Метод, що обраховує та повертає довжину кола
     public double getLength() {
         return 2 * Math.PI * radius;
     }
 
+    // Метод, що обраховує та повертає число PI
     public double getPI() {
         return getLength() / (2 * radius);
     }
--- src/main/java/slack_task_13/Main.java
+++ src/main/java/slack_task_13/Main.java
@@ -2,13 +2,16 @@ package slack_task_13;
 
 public class Main {
     public static void main(String[] args) {
+        // Створення об’єкта та виклик його метода. Назва об’єкта довільна та неприв’язана до контексту
         Circle circle1 = new Circle(12.01);
         System.out.println(circle1.getLength());
 
+        // Створення об’єкта та виклик його метода. Назва об’єкта довільна та неприв’язана до контексту
         Circle circle2 = new Circle(0.5454);
-        System.out.println(circle1.getSquare());
+        System.out.println(circle2.getSquare());
 
+        // Створення об’єкта та виклик його метода. Назва об’єкта довільна та неприв’язана до контексту
         Circle circle3 = new Circle(17.711);
-        System.out.println(circle1.getPI());
+        System.out.println(circle3.getPI());
     }
 }
