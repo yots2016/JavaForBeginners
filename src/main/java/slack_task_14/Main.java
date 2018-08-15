2963758ce68b32380e39ab713d6c8563f06b9e71
Viacheslav <newstageja@gmail.com>
2018-08-15 11:02:12 +0300

    added comments

--- src/main/java/slack_task_14/Main.java
+++ src/main/java/slack_task_14/Main.java
@@ -5,15 +5,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Оголошення змінної типу List та створення об’єкту типу ArrayList
         List<Animal> animals = new ArrayList<>();
 
+        // Додавання в ArrayList об’єктів типу Animal
         animals.add(new Dog("bones", "Ukraine", "Low"));
         animals.add(new Cat("mice", "USA", "High"));
         animals.add(new Horse("grass", "Japan", "Middle"));
 
+        // Створення об’єкта типу Veterinarian
         Veterinarian veterinarian = new Veterinarian();
 
+        // Перебір елементів ArrayList
         for (Animal animal : animals) {
+            // Передавання методу treatAnimal об’єкта типу Veterinarian посилання на об’єкт типу Animal
             veterinarian.treatAnimal(animal);
             System.out.println(animal);
             System.out.println();
