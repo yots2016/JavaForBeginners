0d113b6f8e04fd9faeba7912c8dcde352669113f
Viacheslav <newstageja@gmail.com>
2018-08-17 23:57:57 +0300

    fixed task 13

--- src/main/java/slack_task_13/Circle.java
+++ src/main/java/slack_task_13/Circle.java
@@ -1,26 +1,29 @@
 package slack_task_13;
 
 public class Circle {
+    // Статичне поле класу, константа типу double
+    private static final double PI = 3.14159265359;
+
+    // Поле класу
+    private double radius;
+
     // Конструктор класу
     public Circle(double radius) {
         this.radius = radius;
     }
 
-    // Поле класу
-    private double radius;
-
     // Метод, що обраховує та повертає площу
     public double getSquare(){
-        return Math.PI * Math.pow(radius, 2);
+        return PI * Math.pow(radius, 2);
     }
 
     // Метод, що обраховує та повертає довжину кола
     public double getLength() {
-        return 2 * Math.PI * radius;
+        return 2 * PI * radius;
     }
 
     // Метод, що обраховує та повертає число PI
     public double getPI() {
-        return getLength() / (2 * radius);
+        return PI;
     }
 }
