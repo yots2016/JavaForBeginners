e11b6a7ba1b61d209630413729ed9fabed9c5e7f
Viacheslav Otamas <newstageja@gmail.com>
2018-09-13 12:09:52 +0300

    slack task 15.1 is updated

--- src/main/java/slack_task_15/subtask2/Main.java
+++ src/main/java/slack_task_15/subtask2/Main.java
@@ -1,4 +1,32 @@
 package slack_task_15.subtask2;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int stringsNumber;
+        do {
+            System.out.println("Enter the number of strings from 1: ");
+            stringsNumber = Integer.parseInt(scanner.nextLine());
+        } while (stringsNumber <= 0);
+        System.out.printf("Enter %d strings:%n", stringsNumber);
+
+        String[] strings = new String[stringsNumber];
+
+        for (int i = 0; i < stringsNumber; i++) {
+            strings[i] = scanner.nextLine();
+        }
+
+        Comparator<String> asc = Comparator.comparing(String::length,Integer::compareTo);
+        Comparator<String> desc = Comparator.comparing(String::length, Integer::compareTo).reversed();
+
+        Arrays.sort(strings, asc);
+        System.out.printf("Asc sorted strings - %s.%n", Arrays.toString(strings));
+        Arrays.sort(strings, desc);
+        System.out.printf("Desc sorted strings - %s.", Arrays.toString(strings));
+    }
 }
