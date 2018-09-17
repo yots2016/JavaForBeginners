d48c1f693e89c47ffc52ba47facc131421c05935
Viacheslav Otamas <newstageja@gmail.com>
2018-09-17 09:33:56 +0300

    comments added to slack task 15.1

--- src/main/java/slack_task_15/subtask1/Main.java
+++ src/main/java/slack_task_15/subtask1/Main.java
@@ -4,21 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Створюємо об'єкт класа Scanner передаємо йому як параметр InputStream
         Scanner scanner = new Scanner(System.in);
 
+        //Локальна змінна для зьерігання числа рядків
         int stringsNumber;
+        //Цикл, що зчитує вказану к-сть рядків. В умові виходу перевіряється корекність
+        //вказаної к-сті рядків
         do {
             System.out.println("Enter the number of strings from 1: ");
+            //Присвоюється кількість ріядків
             stringsNumber = Integer.parseInt(scanner.nextLine());
+            //Умова виходу з циклу
         } while (stringsNumber <= 0);
         System.out.printf("Enter %d strings:%n", stringsNumber);
 
+        //Ініціалізація першого рядка та визначення його довжини
         String s = scanner.nextLine();
         int length = s.length();
 
+        //Ініціалізація локальних змінних
         String minString = s, maxString = s;
         int min = length, max = length;
 
+        //Цикл, який зчитує введені рядки та одночасно визначає
+        //максимальний та мінімальний елементи
         for (int i = 0; i < stringsNumber - 1; i++) {
             s = scanner.nextLine();
             length = s.length();
@@ -35,6 +45,7 @@ public class Main {
         }
 
         System.out.println();
+        //Виведення результатів
         System.out.printf("Max string - \"%s\", length - %d.%n", maxString, max);
         System.out.printf("Min string - \"%s\", length - %d.", minString, min);
     }
