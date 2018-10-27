3932d006e5968ce65bbda654d8209ada27b8e447
yots2016 <nestageja@gmail.com>
2018-10-28 02:59:40 +0300

    T2-2 Improve Main class logic

--- src/main/java/special_tasks2/task2/Main.java
+++ src/main/java/special_tasks2/task2/Main.java
@@ -2,103 +2,72 @@ package special_tasks2.task2;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
-    static class MyCallable implements Callable<Integer> {
-        int firstIndex, lastIndex, sum = 0;
-        Integer[] numbers;
+    private final static int MAX_NUMBER = 180;
 
-        public MyCallable(int firstIndex, int lastIndex, Integer[] numbers) {
-            this.firstIndex = firstIndex;
-            this.lastIndex = lastIndex;
-            this.numbers = numbers;
-        }
-
-        @Override
-        public Integer call() {
-            for (int i = firstIndex; i <= lastIndex; i++) {
-                sum += numbers[i];
-            }
-
-            return sum;
+    public static void main(String[] args) {
+        try {
+            System.out.println(sum(MAX_NUMBER));
+        } catch (ExecutionException | InterruptedException e) {
+            throw new IllegalStateException();
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println(sum(180));
+    private static int sum(int maxNumber) throws ExecutionException, InterruptedException {
+        final List<Integer> integers = IntStream.rangeClosed(0, maxNumber)
+                .boxed()
+                .collect(Collectors.toList());
+
+        List<Callable<Integer>> callables = divideListByNumberOfThreads(integers);
+
+        return getSumOfNumbersThroughExecutorService(callables);
     }
 
-    public static long sum(int inputNumber) throws InterruptedException {
-        Integer[] integers = createArray(inputNumber);
+    private static List<Callable<Integer>> divideListByNumberOfThreads(List<Integer> integers) {
+        int threadsNumber = Runtime.getRuntime().availableProcessors();
 
-        initArray(integers);
-
-        return getElementsSum(integers);
-    }
-
-    public static Integer[] createArray(int inputNumber) {
-        if (inputNumber < 0) {
-            return new Integer[1];
-        } else {
-            return new Integer[inputNumber + 1];
-        }
-    }
-
-    public static void initArray(Integer[] integers) {
-        for (int i = 0; i < integers.length; i++) {
-            integers[i] = i;
-        }
-    }
-
-    public static long getElementsSum(Integer[] integers) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
         List<Callable<Integer>> callables = new ArrayList<>();
 
-        processNumbers(integers, callables);
+        final int integersSize = integers.size();
+        final int remainder = integersSize % threadsNumber;
+        final int integersSizeWithoutRemainder = integersSize - remainder;
+        final int step = integersSize / threadsNumber;
 
-        long sum = sumResults(executorService, callables);
+        if (step != 0) {
+            for (int i = 0; i < integersSizeWithoutRemainder; i += step) {
+                int finalI = i;
 
-        executorService.shutdown();
-        return sum;
-    }
-
-    public static void processNumbers(Integer[] integers, List<Callable<Integer>> callables) {
-        int threadsNumber = Runtime.getRuntime().availableProcessors();
-        int integersLength = integers.length;
-        int step = integersLength / threadsNumber;
-        int remainder = integersLength % threadsNumber;
-
-        if (integersLength % threadsNumber == 0) {
-            partitionArray(integers, callables, step, 0);
-        } else {
-            partitionArray(integers, callables, remainder - 1,0);
-
-            if (step != 0) {
-                partitionArray(integers, callables, step, remainder);
+                callables.add(() -> integers.subList(finalI, finalI + step).stream()
+                        .reduce(0, (n1, n2) -> n1 + n2));
             }
         }
+
+        if (remainder != 0) {
+            callables.add(() -> integers.subList(integersSizeWithoutRemainder, integersSize).stream()
+                    .reduce(0, (n1, n2) -> n1 + n2));
+        }
+        return callables;
     }
 
-    public static void partitionArray(Integer[] integers, List<Callable<Integer>> callables, int step, int inputFirstIndex) {
+    private static int getSumOfNumbersThroughExecutorService(List<Callable<Integer>> callables) throws InterruptedException, ExecutionException {
         int threadsNumber = Runtime.getRuntime().availableProcessors();
 
-        for (int i = 0, firstIndex = inputFirstIndex, lastIndex = (step + firstIndex - 1); i < threadsNumber; i++, firstIndex += step, lastIndex += step) {
-            callables.add(new MyCallable(firstIndex, lastIndex, integers));
+        ExecutorService executorService = Executors.newFixedThreadPool(threadsNumber);
+
+        int result = 0;
+
+        for (Future<Integer> integerFuture : executorService.invokeAll(callables)) {
+            result += integerFuture.get();
         }
+
+        executorService.shutdown();
+
+        return result;
     }
 
-    public static long sumResults(ExecutorService executorService, List<Callable<Integer>> callables) throws InterruptedException {
-        return executorService.invokeAll(callables).stream()
-                .map(integerFuture -> {
-                    try {
-                        return integerFuture.get();
-                    } catch (Exception e) {
-                        throw new IllegalStateException(e);
-                    }
-                })
-                .reduce(0, (n1, n2) -> n1 + n2);
-    }
+
 }
