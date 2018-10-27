52e189454401d0a972f8ff22c8f4b1fb28fdb6dc
yots2016 <nestageja@gmail.com>
2018-10-27 22:47:42 +0300

    Removed idea files from staging

--- src/main/java/practice/n1/Main.java
+++ src/main/java/practice/n1/Main.java
@@ -1,4 +1,46 @@
 package practice.n1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("1", "2", "3"));
+
+        ListMultiplicator.multiply(list, 2);
+
+        System.out.println(list);
+    }
+}
+
+/**
+ * Class to modify
+ */
+class ListMultiplicator {
+
+    /**
+     * Multiplies original list provided numper of times
+     *
+     * @param list list to multiply
+     * @param n    times to multiply, should be zero or greater
+     */
+    public static void multiply(List<?> list, int n) {
+        // Add implementation here
+
+        if (n == 0) {
+            list.clear();
+        } else {
+            multiplyHelper(list, n);
+        }
+
+    }
+
+    private static <T> void multiplyHelper(List<T> list, int n) {
+        List<T> source = new ArrayList<>(list);
+        for (int i = 0; i < n - 1; i++) {
+            list.addAll(source);
+        }
+    }
 }
